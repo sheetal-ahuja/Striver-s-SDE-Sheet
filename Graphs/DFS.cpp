4743e1c7ffd9e66233bf4476bbb8f3397f53292b
Sheetal Ahuja <137155289+sheetal-ahuja@users.noreply.github.com>
2024-06-15 17:14:38 +0530

    Update DFS.cpp

--- Graphs/DFS.cpp
+++ Graphs/DFS.cpp
@@ -1,1 +1,45 @@
+// link to problem -> https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
 
+class Solution {
+  public:
+  
+  void DFS(unordered_map<int, vector<int>> &adj, int u,vector<bool> &visited, vector<int>&result)
+  {
+      if(visited[u]==true){
+          return ;
+      }
+      
+      visited[u]=true;
+      result.push_back(u);
+      
+      for(int & v: adj[u]){
+          if(!visited[v]){
+              DFS(adj,v,visited,result);
+          }
+      }
+  }
+  
+  
+  
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int v, vector<int> mp[]) {
+        // Code here
+        unordered_map<int, vector<int>> adj;
+        
+        for(int u=0;u<v;u++)
+        {
+            for(auto v= mp[u].begin(); v!=mp[u].end();v++)
+            {
+                adj[u].push_back(*v);            
+            }
+        }
+        
+        vector<int> result;
+        vector<bool>visited(v,false);
+        
+        
+        DFS(adj,0,visited,result);
+        return result;
+        
+    }
+};
