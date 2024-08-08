6200fb8545d682a423ac9541a30677ae0bd65e77
Sheetal Ahuja <137155289+sheetal-ahuja@users.noreply.github.com>
2024-08-08 17:36:06 +0530

    Update set_matrix_zero.cpp

--- Arrays/set_matrix_zero.cpp
+++ Arrays/set_matrix_zero.cpp
@@ -1,1 +1,95 @@
+// Brute force --> where complexity is n^3 because of which it gives run time error
+
+class Solution {
+public:
+
+    int n, m;
+    void markrow(vector<vector<int>> &matrix, int n, int m, int i)
+    {
+        for(int j=0; j<m;i++)
+        {
+            if(matrix[i][j]!=0)
+            {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+    void markcol(vector<vector<int>> &matrix,int n, int m , int j)
+    {
+        for(int i =0; i<n;i++)
+        {
+            if(matrix[i][j]!=0)
+            {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        n = matrix.size();
+        m = matrix[0].size();
+      for(int i=0; i<n;i++)  
+      {
+        for(int j=0;j<m;j++)
+        {
+            if(matrix[i][j]==0)
+            {
+                markrow(matrix,n,m,i);
+                markcol(matrix,n,m,j);
+            }
+        }
+      }
+
+      for(int i=0; i<n;i++)  
+      {
+        for(int j=0;j<m;j++)
+        {
+            if(matrix[i][j]==-1)
+            {
+               matrix[i][j]=0;
+            }
+        }
+      }
+      
+    }
+};
+
+// Approach with time complexity n^2 which gives run time error
+
+class Solution {
+public:
+    int n,m;
+    void setZeroes(vector<vector<int>>& matrix) {
+        n = matrix.size();
+        m = matrix[0].size();
+        vector<int> row={0};
+        vector<int> col={0};
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+
+        for(int i=0; i<n;i++){
+            for(int j=0;j<m;j++)
+            {
+                if(row[i]||col[j])
+                {
+                    matrix[i][j]=0;
+                }
+            }
+
+        }
+    }
+};
+
+//Approach with Time complexity 
 
