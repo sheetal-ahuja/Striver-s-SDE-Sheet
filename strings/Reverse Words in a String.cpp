327970f726df18269d2be6dca3aba7ff81b7933a
Sheetal Ahuja <137155289+sheetal-ahuja@users.noreply.github.com>
2024-10-23 01:11:59 +0530

    Create Reverse Words in a String.cpp

---
+++ strings/Reverse Words in a String.cpp
@@ -0,0 +1,40 @@
+    string reverseWords(string s) {
+      // -------------------------------------------
+        stringstream ss(s);
+        string token = "";
+        string result = "";
+
+        while(ss>> token)
+        {
+            result = token + " " + result;
+        }
+        
+        return result;
+//---------------------------------------------------------------
+        reverse(s.begin(), s.end());
+        int i = 0;
+        int l = 0; 
+        int r = 0;
+
+        int n = s.length();
+
+        while(i<n)
+        {
+            while(i<n && s[i]!= ' ')
+            {
+                s[r++] = s[i++];
+            }
+
+            if(l<r)
+            {
+                reverse(s.begin()+l, s.begin()+r);
+                s[r] = ' ';
+                r++;
+                l = r;
+            }
+            i++;
+        }
+        s=s.substr(0,r-1);
+        return s;
+    }
+};
